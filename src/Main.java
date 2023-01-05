1341525cd4c43af7ec754c547c0e0bc6b3006171
osj2d <juniorsavi97@gmail.com>
2023-01-04 22:32:09 -0300

    Ta dificil dessa vez

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,22 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+
+
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        String apikey  = null; //Cola a apiKey da conta do imdb-api
+        String apikey  = ""; //Cola a apiKey da conta do imdb-api
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://imdb-api.com/en/API/Top250Movies/"+apikey)).header("Content-type", "application/json").GET().build();
+                .uri(URI.create("https://imdb-api.com/en/API/Top250Movies/"+apikey)).build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String teste = response.body();
         System.out.println(response.body());
+        String[] part = teste.split("\\},\\{");
+        System.out.println(part.length);
+
+
 
 
     }
